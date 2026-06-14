84431f440ebc936efe7df0a8bbd241ff435f1975
ArinaMamai <arina@trytandem.ai>
2026-06-14 14:41:02 +0300

    Update settings

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.v1 import api_router
 from app.core.logging_config import setup_logging
+from app.core.config import settings
 
 setup_logging()
 
@@ -15,14 +16,13 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # В продакшене указать конкретные домены
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Подключаем роутеры
-app.include_router(api_router, prefix="/api/v1")
+app.include_router(api_router, prefix= "/api/v1" if settings.LOCAL == 'true' else '/v1')
 
 
 @app.get("/")
